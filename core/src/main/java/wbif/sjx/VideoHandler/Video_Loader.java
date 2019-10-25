ef354fabcc9c4431b7756d758db8b4f12c4f86c7
Stephen Cross <stephencross86@gmail.com>
2019-10-25 14:00:12 +0100

    First version
    Moved video loading over from MIA (v0.11.15), but made it so the basic functionality shouldn't require MIA.  This should allow the plugin to function on its own.
    
    Former-commit-id: 7168bc5485d55effe544a9fc845b61bfb1c901ab

---
+++ core/src/main/java/wbif/sjx/VideoHandler/Video_Loader.java
@@ -0,0 +1,37 @@
+package wbif.sjx.VideoHandler;
+
+import ij.ImagePlus;
+import ij.gui.GenericDialog;
+import ij.io.OpenDialog;
+import ij.plugin.PlugIn;
+
+import java.io.File;
+
+public class Video_Loader implements PlugIn {
+    @Override
+    public void run(String path) {
+        // Checking if file exists; if not, opening a file selection dialog
+        if (!new File(path).exists()) {
+            OpenDialog openDialog = new OpenDialog("Select video to load",null);
+            path = openDialog.getDirectory()+openDialog.getFileName();
+        }
+
+        // Show parameter selection dialog
+        GenericDialog genericDialog = new GenericDialog("SVG dimensions");
+        genericDialog.addStringField("Frame range","1-end");
+        genericDialog.addStringField("Channel range","1-end");
+        genericDialog.showDialog();
+
+        if (genericDialog.wasCanceled()) return;
+
+        String frameRange = genericDialog.getNextString();
+        String channelRange = genericDialog.getNextString();
+
+        // Do file loading
+        try {
+            VideoLoaderCore.getVideo(path,frameRange,channelRange,null).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
