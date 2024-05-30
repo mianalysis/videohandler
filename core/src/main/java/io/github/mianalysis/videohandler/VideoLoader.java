9d2a60213a4801cc4f4164b6af262b120988190d
Stephen Cross <stephencross86@gmail.com>
2024-05-30 14:34:24 +0100

    Moving to v1.6.0

--- core/src/main/java/io/github/mianalysis/videohandler/VideoLoader.java
+++ core/src/main/java/io/github/mianalysis/videohandler/VideoLoader.java
@@ -36,6 +36,7 @@ import io.github.mianalysis.mia.object.parameters.text.TextAreaP;
 import io.github.mianalysis.mia.object.refs.collections.ImageMeasurementRefs;
 import io.github.mianalysis.mia.object.refs.collections.MetadataRefs;
 import io.github.mianalysis.mia.object.refs.collections.ObjMeasurementRefs;
+import io.github.mianalysis.mia.object.refs.collections.ObjMetadataRefs;
 import io.github.mianalysis.mia.object.refs.collections.ParentChildRefs;
 import io.github.mianalysis.mia.object.refs.collections.PartnerRefs;
 import io.github.mianalysis.mia.object.system.Status;
@@ -475,6 +476,11 @@ public class VideoLoader extends Module {
 
     @Override
     public ObjMeasurementRefs updateAndGetObjectMeasurementRefs() {
+        return null;
+    }
+
+    @Override
+    public ObjMetadataRefs updateAndGetObjectMetadataRefs() {
         return null;
     }
 
